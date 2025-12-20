f227e10512ec4c009ec7f62287935387506589b2
Zuhaitz-dev <200231861+Zuhaitz-dev@users.noreply.github.com>
2025-12-20 11:48:49 +0000

    Auto-update: Bump z-core and regenerate zthread.h [skip ci]

--- zthread.h
+++ zthread.h
@@ -146,6 +146,7 @@
 #endif // Z_COMMON_BUNDLED
 /* ============================================================================ */
 
+
 /*
  * zthread.h — Portable threads, mutexes, and synchronization primitives
  * Part of Zen Development Kit (ZDK)
@@ -165,7 +166,7 @@
  * License: MIT
  * Author: Zuhaitz
  * Repository: https://github.com/z-libs/zthread.h
- * Version: 1.1.0
+ * Version: 1.1.2
  */
 
 #ifndef ZTHREAD_H
@@ -183,6 +184,7 @@
 #else
 #   include <pthread.h>
 #   include <unistd.h>
+#   include <time.h>
     typedef pthread_t zthread_t;
     typedef pthread_mutex_t zmutex_t;
     typedef pthread_cond_t zcond_t;
@@ -520,7 +522,11 @@ namespace z_thread
 
 #endif // __cplusplus
 
+#endif // ZTHREAD_H
+
 #ifdef ZTHREAD_IMPLEMENTATION
+#ifndef ZTHREAD_IMPLEMENTATION_GUARD
+#define ZTHREAD_IMPLEMENTATION_GUARD
 
 struct zthread__wrap 
 { 
@@ -656,7 +662,10 @@ void zthread_detach(zthread_t t)
 
 void zthread_sleep(int ms) 
 { 
-    usleep(ms * 1000); 
+    struct timespec ts;
+    ts.tv_sec = ms / 1000;
+    ts.tv_nsec = (ms % 1000) * 1000000;
+    nanosleep(&ts, NULL);
 }
 
 void zmutex_init(zmutex_t *m) 
@@ -704,5 +713,6 @@ void zcond_destroy(zcond_t *c)
 }
 #endif
 
+#endif // ZTHREAD_IMPLEMENTATION_GUARD
+
 #endif // ZTHREAD_IMPLEMENTATION
-#endif // ZTHREAD_H
